fc6abe3f948ff2f2ee56e0f4ccf872104907cb6a
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2018-10-27 14:55:44 +0200

    Embedded sort

--- src/main/java/pl/sda/EmbeddedSort.java
+++ src/main/java/pl/sda/EmbeddedSort.java
@@ -1,18 +1,21 @@
 package pl.sda;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmbeddedSort {
     public static void main(String[] args) {
 
         List<Integer> numbers = new ArrayList<>();
         generateNumbers();
-        sort();
+        sort(numbers);
         System.out.println(numbers);
     }
 
-    private static void sort() {
+    private static void sort(List<Integer> numbers) {
+        Collections.sort(numbers);
     }
 
     private static void generateNumbers() {
